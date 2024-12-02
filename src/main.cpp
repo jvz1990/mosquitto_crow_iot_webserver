0c6c1a29ea615985536431d2301a13a78f4793c3
jvz1990 <jvz__@hotmail.com>
2024-12-02 12:53:01 +1100

    Added crow to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <fmt/core.h>
+#include "crow.h"
 
 int main() {
     fmt::print("Hello World!\n");
-    return 0;
+
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    app.port(18080).multithreaded().run();
 }
